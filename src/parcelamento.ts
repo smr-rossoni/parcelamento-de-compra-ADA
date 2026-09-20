961fecfe6972e486b09fd1887a226ad22e41a342
Samara Rossoni <rossonisamara@gmail.com>
2026-09-20 20:39:21 -0300

    feat: implementa juros de 5% para 5x a 8x - GREEN

--- src/parcelamento.ts
+++ src/parcelamento.ts
@@ -7,8 +7,12 @@ export function calcularParcelamento(
   valorCompra: number,
   numeroParcelas: number
 ): ResultadoParcelamento {
+  let juros = 0
+  if (numeroParcelas >= 5 && numeroParcelas <= 8) juros = 0.05
+
+  const total = valorCompra * (1 + juros)
   return {
-    valorParcela: valorCompra / numeroParcelas,
+    valorParcela: total / numeroParcelas,
     totalParcelas: numeroParcelas,
   }
 }
